511fdc47c6826829868f87cf22d814b9209dd810
onekarma <stephen.to@onekarma.org>
2012-10-27 16:55:12 -0700

    Added a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "onekarma" }
-
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Home")
+                        :text => "onekarma")
     end
-
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
 
   describe "Story page" do
@@ -25,13 +25,15 @@ describe "Static pages" do
       visit '/static_pages/stories'
       page.should have_content('Stories')
     end
-
-    it "should have the title 'Stories'" do
-      visit '/static_pages/stories'
+    it "should have the base title" do
+      visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Stories")
+                        :text => "onekarma")
     end
-
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Stories')
+    end
   end
 
   describe "Contact page" do
@@ -40,11 +42,14 @@ describe "Static pages" do
       visit '/static_pages/contact'
       page.should have_content('Contact')
     end
-
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+    it "should have the base title" do
+      visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Contact")
+                        :text => "onekarma")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Contact')
     end
   end
 
@@ -54,11 +59,14 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_content('About onekarma')
     end
-
-    it "should have the title 'About onekarma'" do
-      visit '/static_pages/about'
+    it "should have the base title" do
+      visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | About")
+                        :text => "onekarma")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| About')
     end
   end
 
